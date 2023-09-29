e363b0bf0ab0099ccecb23c163d41f3ab71df617
heisenberg <owentobias21@gmail.com>
2023-09-30 01:05:49 +0700

    update inversMatriks

--- src/General/InversMatriks.java
+++ src/General/InversMatriks.java
@@ -1,18 +1,21 @@
 package General;
+
 import java.util.Scanner;
 
-
 public class InversMatriks {
+    public static Scanner input;
 
-    // Melakukan operasi inverse terhadap matrix dengan menggunakan metode Gauss Jordan
+    // Melakukan operasi inverse terhadap matrix dengan menggunakan metode Gauss
+    // Jordan
     public static double[][] inverseGaussJordan(double[][] matrix) {
         // KAMUS LOKAL
         int n = matrix.length;
         double pengkalian;
-        
+
         // Membuat sebuah matrix identitas
         double[][] inverse = OperasiMatriks.CreateMatrixIdentitas(matrix);
         
+
         // Menggunakan Gauss-Jordan elimination
         for (int i = 0; i < n; i++) {
             // Cari baris dengan elemen terbesar di kolom i sebagai pivot
@@ -37,19 +40,19 @@ public class InversMatriks {
             }
             
             pengkalian = matrix[i][i];
-            
+
             // Cek apakah matriks singular atau tidak
-            if (pengkalian == 0.0) {
+            if (pengkalian == 0.0f) {
                 System.out.println("Matrix tersebut singular, tidak bisa diinverse.");
                 return null;
             }
-            
-            // Mengalikan baris dengan elemen seterusnya
+
+            // Mengalikan row dan elemen seterusnya
             for (int j = 0; j < n; j++) {
                 matrix[i][j] /= pengkalian;
                 inverse[i][j] /= pengkalian;
             }
-            
+
             // Eliminasi elemen lain
             for (int k = 0; k < n; k++) {
                 if (k != i) {
@@ -61,10 +64,10 @@ public class InversMatriks {
                 }
             }
         }
-        
+
         return inverse;
     }
-    
+
     // Melakukan operasi inverse terhadap matrix dengan menggunakan metode Adjoint
     public static double[][] inverseAdjoint(double[][] matrix) {
         double determinant = OperasiMatriks.determinanKofaktor(matrix);
@@ -93,10 +96,10 @@ public class InversMatriks {
         
         return inverse;
     }
-    
+
     // Print rumus inverse Gauss-Jordan
     public static void printGauss(double[][] matrix) {
-        //KAMUS LOKAL
+        // KAMUS LOKAL
         int i, j;
 
         // ALGORITMA
@@ -105,35 +108,35 @@ public class InversMatriks {
         double[][] identitas = OperasiMatriks.CreateMatrixIdentitas(matrix);
         for (i = 0; i < matrix.length; i++) {
             System.out.print("|");
-            for (j = 0; j < matrix[i].length;j++) {
+            for (j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j]);
-                if(j != matrix[i].length-1) {
+                if (j != matrix[i].length - 1) {
                     System.out.print(" ");
                 }
             }
             System.out.print("|");
-            for (j = 0; j < identitas[i].length;j++) {
+            for (j = 0; j < identitas[i].length; j++) {
                 System.out.print(identitas[i][j]);
-                if(j != identitas[i].length-1) {
+                if (j != identitas[i].length - 1) {
                     System.out.print(" ");
                 }
             }
             System.out.print("|");
             System.out.println();
         }
-               System.out.println();
+        System.out.println();
         for (i = 0; i < identitas.length; i++) {
             System.out.print("|");
-            for (j = 0; j < identitas[i].length;j++) {
+            for (j = 0; j < identitas[i].length; j++) {
                 System.out.print(identitas[i][j]);
-                if(j != identitas[i].length-1) {
+                if (j != identitas[i].length - 1) {
                     System.out.print(" ");
                 }
             }
             System.out.print("|");
-            for (j = 0; j < verse[i].length;j++) {
+            for (j = 0; j < verse[i].length; j++) {
                 System.out.print(verse[i][j]);
-                if(j != verse[i].length-1) {
+                if (j != verse[i].length - 1) {
                     System.out.print(" ");
                 }
             }
@@ -141,56 +144,61 @@ public class InversMatriks {
             System.out.println();
         }
     }
-    
-    // sub menu inverse
-    public static void main(String[] args) {
-        // ALGORITMA
-        double[][] matrix = null;
-        double[][] inverse = null;
-        Scanner input = new Scanner(System.in);
-        String metode = null;
-        while (true) {
-            System.out.println("Metode Inverse:\n1. Metode Gauss Jordan\n2. Metode Adjoin");
-            System.out.print("Metode: ");
-            String via = input.nextLine();
-            if (via.equals("1")){
-                metode = "1";
-                break;
-            } else if (via.equals("2")) {
-                metode = "2";
-                break;
-            } else {
-                System.out.println("Input salah harap untuk memasukkan pilihan yang tersedia.");
-            }
-            }
-        while (true) {
-            System.out.println("Input matrix via:\n1. File\n2. User");
-            System.out.print("Via: ");
-            String via = input.nextLine();
-            if (via.equals("1") ){
-                matrix = Fungsi.inputFromTxt();
-                break;
-            } else if (via.equals("2")) {
-                matrix = Fungsi.inputFromUser();
-                break;
-            } else {
-                System.out.println("Input salah harap untuk input hanya \"file\" atau \"user\".");
-            }
-        }
-        input.close();
 
-        if (metode == "1") {
-            printGauss(matrix);
-            inverse = inverseGaussJordan(matrix);
-        } else {
-            System.out.println();
-            System.out.println("Inverse matrix = 1 / det(matrix) x Adjoin(matrix)");
-            inverse = inverseAdjoint(matrix);
-        }
-        
-        System.out.println();
-        System.out.println("Inverse Matriks: ");
-        Fungsi.displayMatrix(inverse);
-    }   
+    // sub menu inverse
+    /*
+     * public static void subMenuInverse() {
+     * // ALGORITMA
+     * double[][] matrix = null;
+     * double[][] inverse = null;
+     * Scanner input = new Scanner(System.in);
+     * String metode = null;
+     * while (true) {
+     * System.out.
+     * println("Metode Inverse:\n1. Metode Gauss Jordan\n2. Metode Adjoin");
+     * System.out.print("Metode: ");
+     * String via = input.nextLine();
+     * if (via.equals("1")){
+     * metode = "1";
+     * break;
+     * } else if (via.equals("2")) {
+     * metode = "2";
+     * break;
+     * } else {
+     * System.out.
+     * println("Input salah harap untuk memasukkan pilihan yang tersedia.");
+     * }
+     * }
+     * while (true) {
+     * System.out.println("Input matrix via:\n1. File\n2. User");
+     * System.out.print("Via: ");
+     * String via = input.nextLine();
+     * if (via.equals("1") ){
+     * matrix = Fungsi.inputFromTxt();
+     * break;
+     * } else if (via.equals("2")) {
+     * matrix = Fungsi.inputFromUser();
+     * break;
+     * } else {
+     * System.out.
+     * println("Input salah harap untuk input hanya \"file\" atau \"user\".");
+     * }
+     * }
+     * input.close();
+     * 
+     * if (metode == "1") {
+     * printGauss(matrix);
+     * inverse = inverseGaussJordan(matrix);
+     * } else {
+     * System.out.println();
+     * System.out.println("Inverse matrix = 1 / det(matrix) x Adjoin(matrix)");
+     * inverse = inverseAdjoint(matrix);
+     * }
+     * 
+     * System.out.println();
+     * System.out.println("Inverse Matriks: ");
+     * Fungsi.displayMatrix(inverse);
+     * }
+     */
 
 }
