84a0081dee83c261932f26ddb6c186f94c562162
heisenberg <owentobias21@gmail.com>
2023-10-01 12:43:52 +0700

    update SPL

--- src/special/SPL.java
+++ src/special/SPL.java
@@ -1,19 +1,19 @@
 package Special;
 
 public class SPL {
-    public static String splGauss(double[][] matriks) {
-        return "";
+    public static String[] splGauss(double[][] matriks) {
+        return null;
     }
 
-    public static String splGaussJ(double[][] matriks) {
-        return "";
+    public static String[] splGaussJ(double[][] matriks) {
+        return null;
     }
 
-    public static String splInvers(double[][] matriks) {
-        return "";
+    public static String[] splInvers(double[][] matriks) {
+        return null;
     }
 
-    public static String splCramer(double[][] matriks) {
-        return "";
+    public static String[] splCramer(double[][] matriks) {
+        return null;
     }
 }
