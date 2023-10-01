c9beb145e470893f32e89eefae5a7060442a8a00
heisenberg <owentobias21@gmail.com>
2023-10-01 12:43:02 +0700

    update konversi

--- src/General/Konversi.java
+++ src/General/Konversi.java
@@ -2,6 +2,7 @@ package General;
 
 import java.util.Scanner;
 
+// class ini menyimpan fungsi-fungsi untuk mengkonversi hasil perhitungan menjadi string sesuai format luaran
 public class Konversi {
     public static Scanner input;
 
@@ -25,5 +26,59 @@ public class Konversi {
             return luaran;
         }
 
+    }
+
+    // konversi hasil spl menjadi string sesuai format
+    public static String splKeString(String[] solusi) {
+        String luaran = "";
+        int i;
+        if (solusi[0] != "SPL tidak memiliki solusi.") {
+            luaran = luaran.concat("Solusi SPL: \n");
+        }
+        if (solusi.length == 1) {
+            luaran = luaran.concat(solusi[0]);
+        } else {
+            for (i = 0; i < solusi.length; i++) {
+                luaran = luaran.concat("x" + (i + 1) + " = " + solusi[i] + "\n");
+            }
+        }
+
+        return luaran;
+    }
+
+    // konversi polinom interpolasi menjadi persamaan sesuai format
+    public static String interpolasiKeString(double[] array) {
+        String pangkat, luaran = "";
+        int i;
+        luaran = luaran.concat("Persamaan polinom interpolasi:\n");
+        luaran = luaran.concat("f(x) = ");
+
+        for (i = 1; i <= (int) array[0] + 1; i++) {
+            if (i == (int) array[0]) {
+                pangkat = "x";
+
+            } else if (i == (int) array[0] + 1) {
+                pangkat = "";
+            } else {
+                pangkat = ("x^" + ((int) array[0] + 1 - i));
+
+            }
+            if (array[i] == 0.0) {
+                continue;
+            } else if (array[i] < 0) {
+                array[i] *= -1;
+                luaran = luaran.concat(" - " + array[i] + pangkat);
+            } else {
+                if (i == 1) {
+                    luaran = luaran.concat(array[i] + pangkat);
+                } else {
+                    luaran = luaran.concat(" + " + array[i] + pangkat);
+                }
+            }
+        }
+        i = (int) array[0] + 2;
+        luaran = luaran.concat("\n\nSolusi:\n");
+        luaran = luaran.concat("f(" + array[i] + ") = " + array[i + 1] + "\n");
+        return luaran;
     }
 }
